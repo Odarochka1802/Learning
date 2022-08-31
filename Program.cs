4ee13d08af1c07597b8377c306f6f2a21215297b
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-08-31 19:11:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace learning {
+
+    class Program{
+
+        static void Main(){
+            int num_1 = 0, num_2 = 0;
+
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("First: " + num_1 + ". Second: " + num_2);
+
+
+            //Получение данных
+            //Console.ReadKey();
+            
+        }
+
+    }
+}
